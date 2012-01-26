11877021f59589a30dd89c78d1b63a920e5ad3fb
yemliha ipek <yemlihaipek@gmail.com>
2012-01-26 16:34:46 +0200

    Teacher eklendi

--- src/main/java/com/yemliha/randevum/entity/Teacher.java
+++ src/main/java/com/yemliha/randevum/entity/Teacher.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.yemliha.randevum;
+package com.yemliha.randevum.entity;
 
 /**
  *
  * @author istyemlihai
  */
-public interface Tasit {
+public interface Teacher {
     
 }
